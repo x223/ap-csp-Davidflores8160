0a092c50b4c615ea439550155bba52f46ff7b16c
Davidflores8160 <david.flores@ms223.org>
2016-10-14 14:27:41 -0400

    Add files via upload

---
+++ stop and go/sketch.js
@@ -0,0 +1,15 @@
+var fillcolor = 'red'
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background('white');
+	fill('black')
+	rect(140,20,120,230)
+	fill('white')
+	ellipse(200,60,60,60)
+	ellipse(200,130,60,60)
+	ellipse(200,200,60,60)
+				
+}
